c7827c3b5962d8a23a0df1f41701ee1196065688
Albos Hajdari <36895112+AlbosHajdari@users.noreply.github.com>
2019-12-18 17:02:25 +0100

    Update SkriptaProjektit.sql

--- SkriptaProjektit.sql
+++ SkriptaProjektit.sql
@@ -1,158 +1,90 @@
-CREATE SEQUENCE UserIdSeq START WITH 1 CACHE 2;
-
---Drop SEQUENCE userIdSeq;
-
-
-CREATE OR REPLACE TYPE Fjalekalimi AS Object
-(
-    Pass nvarchar2(30),
-    Salt nvarchar2(30)
-);
-/
-
-
-CREATE OR REPLACE TYPE Adresa AS Object
-(
-    Qyteti nvarchar2(30),
-    Rajoni nvarchar2(30),
-    Rruga nvarchar2(30),
-    Zip nvarchar2(10)
-);
-/
-CREATE OR REPLACE TYPE Lista_e_Adresave 
-AS TABLE OF Adresa;
-/
-
---Drop Table Useri;
-CREATE TABLE Useri(
-    PersonId Number(10) NOT NULL,
-    Emri nvarchar2(30),
-    Mbiemri nvarchar2(30),
-    Email nvarchar2(30),
-    Datelindja DATE,
-    Fjalekalimi Fjalekalimi NOT NULL,
-    Adresa Lista_e_Adresave,
-    CONSTRAINT PersonIdPK PRIMARY KEY (PersonId)
-)   NESTED TABLE Adresa STORE AS Adresa_Nested;
-
-
-CREATE OR REPLACE TRIGGER UserIdCreate
-BEFORE INSERT ON Useri 
-FOR EACH ROW
-BEGIN
-  SELECT UserIdSeq.NEXTVAL
-  INTO   :new.PersonId
-  FROM   dual;
-END;
-/
-
-CREATE OR REPLACE TRIGGER UserIdCreate
-BEFORE INSERT ON Useri 
-FOR EACH ROW
-BEGIN
-  SELECT UserIdSeq.NEXTVAL
-  INTO   :new.PersonId
-  FROM   dual;
-END;
-/
-
-
-Alter TABLE Useri 
-ADD Fjalekalimi Fjalekalimi NOT NULL;
-
-ALTER TABLE Useri
-ADD Adresa Lista_e_adresave NESTED TABLE Adresa STORE AS Adresa_Nested;
-
-
-Insert into Useri (Emri,Mbiemri,Fjalekalimi,Adresa)
-Values ('Albos','asd', Fjalekalimi('asd','123'),Lista_e_adresave(Adresa('Ferizaj','Ferizaj','Kuvendi i Arberit','70000'),Adresa('Kommogllave','Ferizaj','Rr.E Vitise','70000')));
-
-SELECT Emri,Mbiemri,Email,Datelindja,U.Fjalekalimi FROM Useri U;
-SELECT SYSDATE FROM DUAL;
-SELECT * FROM Useri;
-
-Update Useri
-SET Adresa = Lista_e_Adresave( Adresa('Ferizaj','Ferizaj','Kuvendi i Arberit','70000'),Adresa('Kommogllave','Ferizaj','Rr.E Vitise','70000'));
-
-
-TRUNCATE TABLE Useri;
-
-SELECT * FROM DICT;
-
-CREATE TABLE tblDhomeGjumi(
-    DhomaGjumitID Number(10) NOT NULL,
-    LlojiPerdoruesveTeDhomes nvarchar2(30),
-    NumriKrevateve number(2),
-    SiperfaqjaDhomesGjumit number(5,2),
-    --FotoDhomesGjumit PHOTO,
-    --CONSTRAINT FemijeApoTeRriturApoQift ,
+CREATE TABLE tblShtepia (
+    ShtepiaID number(10) NOT NULL,
+    ObjektiID number(10) NOT NULL,
+    SiperfaqjaTotaleShtepise number(8,2) NOT NULL,
+    SiperfaqjaTotaleOborrit number(8,2) NOT NULL,
+    NumriBallkoneve number(1),
+    Lifti char(1) NOT NULL CHECK (Lifti = '1' OR Lifti = '0'),
     
-    PRIMARY KEY (DhomaGjumitID)
+    CONSTRAINT ShtepiaIdPK PRIMARY KEY (ShtepiaID),
+    CONSTRAINT shtepiaObjektiIdFK FOREIGN KEY (ObjektiID) REFERENCES Objekti(ObjektId) ON DELETE CASCADE
 );
 
+
+CREATE TABLE tblApartamenti(
+    ApartamentiID number(10) NOT NULL,
+    ObjektiID number(10) NOT NULL,
+    SiperfaqjaTotaleApartamentit number(8,2) NOT NULL,
+    KatiNumer number(3) NOT NULL,
+    Lifti char(1) NOT NULL CHECK (Lifti = '1' OR Lifti = '0'),
+    NumriBallkoneve number(1),
+    
+    CONSTRAINT ApartamentiIdPK PRIMARY KEY (ApartamentiID),
+    CONSTRAINT apartamentiObjektiIdFK FOREIGN KEY (ObjektiID) REFERENCES Objekti(ObjektId) ON DELETE CASCADE
+);
+
+ 
+CREATE TABLE tblDhomeGjumi(
+    DhomeGjumiID number(10) NOT NULL,
+    ShtepiaID number(10),
+    ApartamentiID number(10),
+    LlojiPerdoruesveTeDhomes nvarchar2(30) CHECK (LlojiPerdoruesveTeDhomes = 'femije' OR LlojiPerdoruesveTeDhomes = 'te rritur' OR LlojiPerdoruesveTeDhomes = 'qift'),
+    NumriKrevateve number(2),
+    
+    CONSTRAINT DhomeGjumiIdPK PRIMARY KEY (DhomeGjumiID),
+    CONSTRAINT dhomeGjumiShtepiIdFK FOREIGN KEY (ShtepiaID) REFERENCES tblShtepia(ShtepiaID) ON DELETE CASCADE,
+    CONSTRAINT dhomeGjumiApartamentiIdFK FOREIGN KEY (ApartamentiID) REFERENCES tblApartamenti(ApartamentiID) ON DELETE CASCADE
+
+);
 
 CREATE TABLE tblDhomeNdejes(
-    DhomaNdejesID Number(10) NOT NULL,
-    NumriDivaneve number(2),
-    SiperfaqjaDhomesNdejes number(5,2),
-    --FotoDhomesNdejes PHOTO,
-    CONSTRAINT DhomaNdejesIdPK PRIMARY KEY (DhomaNdejesID)
+    DhomeNdejesID number(10) NOT NULL,
+    ShtepiaID number(10),
+    ApartamentiID number(10),
+    NumriDivanave number(2),
+    
+    CONSTRAINT DhomeNdejesIdPK PRIMARY KEY (DhomeNdejesID),
+    CONSTRAINT dhomeNdejesShtepiIdFK FOREIGN KEY (ShtepiaID) REFERENCES tblShtepia(ShtepiaID) ON DELETE CASCADE,
+    CONSTRAINT dhomeNdejesApartamentiIdFK FOREIGN KEY (ApartamentiID) REFERENCES tblApartamenti(ApartamentiID) ON DELETE CASCADE
 );
 
 CREATE TABLE tblKuzhina(
-    KuzhinaID Number(10) NOT NULL,
-    TipiNgrohesit nvarchar2(30),
-    TipiFtohesit nvarchar2(30),
-    TavolinaUshqimi boolean,
-    Enet boolean,
-    Enelaresi boolean,
-    SiperfaqjaKuzhines number(5,2),
-    --FotoKuzhines PHOTO,
-    CONSTRAINT KuzhinaIdPK PRIMARY KEY (KuzhinaID)
-);
-
-commit;
-
-CREATE TABLE tblBallkoni(
-    BallkoniID Number(10) NOT NULL,
-    SiperfaqjaBallkonit number(5,2),
-    --FotoBallkonit PHOTO,
-    CONSTRAINT BallkoniIdPK PRIMARY KEY (BallkoniID)
+    KuzhinaID number(10) NOT NULL,
+    ShtepiaID number(10),
+    ApartamentiID number(10),
+    TavolinaUshqimit char(1) NOT NULL CHECK (TavolinaUshqimit = '1' OR TavolinaUshqimit = '0'),
+    Enet char(1) NOT NULL CHECK (Enet = '1' OR Enet = '0'),
+    EneLaresi char(1) NOT NULL CHECK (EneLaresi = '1' OR EneLaresi = '0'),
+    
+    TipiFtohesit char(10) NOT NULL CHECK (TipiFtohesit = 'friz' OR TipiFtohesit = 'frigorifer' OR TipiFtohesit = 'te dyjat'),
+    TipiNgrohesit char(10) NOT NULL CHECK (TipiNgrohesit = 'plloqe' OR TipiNgrohesit = 'furre' OR TipiNgrohesit = 'te dyjat'),
+    
+    
+    CONSTRAINT KuzhinaIdPK PRIMARY KEY (KuzhinaID),
+    CONSTRAINT kuzhinaShtepiIdFK FOREIGN KEY (ShtepiaID) REFERENCES tblShtepia(ShtepiaID) ON DELETE CASCADE,
+    CONSTRAINT kuzhinaApartamentiIdFK FOREIGN KEY (ApartamentiID) REFERENCES tblApartamenti(ApartamentiID) ON DELETE CASCADE
 );
 
 CREATE TABLE tblBanjo(
-    BanjoID Number(10) NOT NULL,
-    SiperfaqjaBanjos number(5,2),
-    PerPastrim boolean,
-    --FotoBanjos PHOTO,
-    CONSTRAINT BanjoIdPK PRIMARY KEY (BanjoID)
+    BanjoID number(10) NOT NULL,
+    ShtepiaID number(10),
+    ApartamentiID number(10),
+    PerPastrim char(1) NOT NULL CHECK (PerPastrim = '1' OR PerPastrim = '0'),
+    
+    CONSTRAINT BanjoIdPK PRIMARY KEY (BanjoID),
+    CONSTRAINT BanjoShtepiIdFK FOREIGN KEY (ShtepiaID) REFERENCES tblShtepia(ShtepiaID) ON DELETE CASCADE,
+    CONSTRAINT BanjoApartamentiIdFK FOREIGN KEY (ApartamentiID) REFERENCES tblApartamenti(ApartamentiID) ON DELETE CASCADE
 );
-commit;
 
 CREATE TABLE tblGarazhda(
-    GarazhdaID Number(10) NOT NULL,
-    SiperfaqjaGarazhdes number(6,2),
-    TipiDyerve nvarchar2(20),
-    --FotoGarazhdes PHOTO,
-    CONSTRAINT GarazhdaIdPK PRIMARY KEY (GarazhdaID)
+    GarazhdaID number(10) NOT NULL,
+    ShtepiaID number(10),
+    ApartamentiID number(10),
+    ObjektiID number(10),
+    HapjaEDyerve char(10) NOT NULL CHECK ( HapjaEDyerve = 'fizike' OR HapjaEDyerve = 'me dirigjim' OR HapjaEDyerve = 'automatike'),
+    
+    CONSTRAINT GarazhdaIdPK PRIMARY KEY (GarazhdaID),
+    CONSTRAINT GarazhdaShtepiIdFK FOREIGN KEY (ShtepiaID) REFERENCES tblShtepia(ShtepiaID) ON DELETE CASCADE,
+    CONSTRAINT GarazhdaApartamentiIdFK FOREIGN KEY (ApartamentiID) REFERENCES tblApartamenti(ApartamentiID) ON DELETE CASCADE,
+    CONSTRAINT GarazhdaObjektiIdFK FOREIGN KEY (ObjektiID) REFERENCES Objekti(ObjektId) ON DELETE CASCADE
 );
-                                                                                              
-___________________________________________________________________________________________________
-                                                                                              
-CREATE TABLE tblApartamentApoKat(
-    ApartamentApoKatiID Number(10) NOT NULL,
-    KatiNumer number(3),
-    Lifti char(1) NOT NULL, --boolean
-    CONSTRAINT aKaLift CHECK ( Lifti = '1' OR Lifti = '0' ),
-    --SiperfaqjaApartamentit number(5,2),
-    Garazhdat garazhdat,
-    CONSTRAINT ApartamentApoKatiIdPK PRIMARY KEY (ApartamentApoKatiID)
-)NESTED TABLE Garazhdat STORE AS atributi_garazhdat
-;
-                                                                                              
-CREATE TYPE garazhda AS OBJECT(
-    SiperfaqjaGarazhdes number(6,2),
-    TipiDyerveTeGarazhdes nvarchar2(20)
-    --FotoGarazhdes PHOTO
-);
-CREATE TYPE garazhdat IS TABLE OF garazhda;                                                                                         
